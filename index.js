92f72bffdbe9014593412342c2a185c868b5c389
Michael <mikahado@gmail.com>
2022-09-02 10:36:21 -0400

    looping code along complete

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
-// Code your solutions in this file
+
+
+function writeCards(names, event) {
+    const messages = []
+    for (let i = 0; i < names.length; i++) {
+    messages.push(`Thank you, ${names[i]}, for the wonderful ${event} gift!`);
+  }
+
+  return messages
+}
+
+function countDown(n) {
+    let count = 11;
+        while (count > 0) {
+        console.log(--count);
+        }
+}
+
+console.log(countDown)
